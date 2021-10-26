21fd435f32d085d77666930ad2192eb94c3dbca6
danzabarr <danzabarr@hotmail.co.uk>
2021-10-26 16:01:37 +0100

    Initial commit

---
+++ Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace torc
+{
+    class Entity
+    {
+        public Transform Transform => (Transform)components[0];
+        private List<Component> components;
+        
+        public Entity()
+        {
+            components = new List<Component>();
+        }
+
+    }
+}
